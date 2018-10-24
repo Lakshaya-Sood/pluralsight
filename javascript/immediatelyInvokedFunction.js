7e88645c94222f3bc2e069edfdc150a9fea5bad3
lakshaya.sood@metronom-external.com <lakshaya.sood@metronom-external.com>
2018-10-24 20:36:39 +0530

    python notes update

--- javascript/immediatelyInvokedFunction.js
+++ javascript/immediatelyInvokedFunction.js
@@ -1,4 +1,6 @@
+//https://blog.mariusschulz.com/2016/01/13/disassembling-javascripts-iife-syntax
 //mainly used to declare non global variables
+//IIFEs can be used to wrap state (or data in general) that's meant to be private
 //in plain JS
 (function(){
 	var num = '123'
@@ -25,3 +27,18 @@ const currentCounterValue = (function() {
 console.log(currentCounterValue()); 
 console.log(currentCounterValue());
 console.log(currentCounterValue());
+
+//passing global variable inside function
+//(function(){}()) --- more better at readability
+//(function(){})()  --- less better at readability
+
+//on browser
+//(function(a,b){
+//	a is window and b is document
+//})(window,document)
+
+//In node
+global.x = '123'; 
+(function(global){
+	console.log(global)
+})(this)
