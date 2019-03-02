2204994740b19acd5fbca068a65fcd33a80431fd
lakshaya.sood@metronom-external.com <lakshaya.sood@metronom-external.com>
2019-03-02 16:21:34 +0530

    some change

--- typescript/tip_5.ts
+++ typescript/tip_5.ts
@@ -1,7 +1,7 @@
 // TIP #5 -> Using spread operator is a great way to go
 function boo(x, y, z) {console.log(x,y,z) }
 var args123 = [0, 1, 2];
-//boo(...args123); // works in es6 but not in typescript
+//boo(...args123); // works in es6 but not in typescript ??
 
 var xio = [4,5,6]
 args123.push(...xio) // Adds arr2 items to end of array
